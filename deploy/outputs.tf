6f4d186dab81328e084f39fae3106a6f1dde5c1f
rafiqirosli <fricheps@Hotmail.com>
2025-09-22 23:34:12 +0930

    Update outputs.tf

--- deploy/outputs.tf
+++ deploy/outputs.tf
@@ -3,27 +3,7 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "public_subnet_id" {
-  description = "The ID of the public subnet"
-  value       = module.subnets.public_subnet_id
-}
-
-output "private_subnet_id" {
-  description = "The ID of the private subnet"
-  value       = module.subnets.private_subnet_id
-}
-
 output "vpc_cidr" {
   description = "CIDR of the VPC"
   value       = aws_vpc.main.cidr_block
-}
-
-output "public_subnet_cidr" {
-  description = "CIDR of the public subnet"
-  value       = module.subnets.public_subnet_cidr
-}
-
-output "private_subnet_cidr" {
-  description = "CIDR of the private subnet"
-  value       = module.subnets.private_subnet_cidr
 }
