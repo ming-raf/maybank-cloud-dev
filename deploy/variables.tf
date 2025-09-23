145f8f04eec3913b9c9570dd78b92dfd02ea05ac
rafiqirosli <fricheps@Hotmail.com>
2025-09-23 22:58:50 +0930

    Update variables.tf

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -18,18 +18,3 @@ variable "VERSION" {
   description = "Version of the deployment"
   type        = string
 }
-
-variable "route53_zone_id" {
-  description = "Route53 hosted zone ID that contains the domain"
-  type        = string
-}
-
-variable "origin_tls_certificate_arn" {
-  description = "ACM certificate ARN for the NLB TLS listener (region must match the NLB region)"
-  type        = string
-}
-
-variable "nlb_dns_name" {
-  description = "DNS name of the existing Network Load Balancer to use as CloudFront origin (e.g., my-nlb-123456.elb.ap-southeast-1.amazonaws.com)"
-  type        = string
-}
