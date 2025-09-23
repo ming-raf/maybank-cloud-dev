18ff0c5abfedd687aba20e8e7ea5df832dd10cf6
rafiqirosli <fricheps@Hotmail.com>
2025-09-23 23:27:21 +0930

    Update nlb.tf

--- deploy/nlb.tf
+++ deploy/nlb.tf
@@ -28,49 +28,13 @@ resource "aws_lb_target_group" "nlb_tg" {
 
 resource "aws_lb_listener" "nlb_https" {
   load_balancer_arn = aws_lb.nlb.arn
-  port              = 443
-  protocol          = "TLS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.nlb.arn
-
-  depends_on = [aws_acm_certificate_validation.nlb]
+  port              = 80
+  protocol          = "TCP"
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.nlb_tg.arn
   }
-}
-
-resource "aws_acm_certificate" "nlb" {
-  domain_name       = local.domain_name
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "nlb_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.nlb.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = aws_route53_zone.edge.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 300
-  records = [each.value.record]
-
-  allow_overwrite = true
-}
-
-resource "aws_acm_certificate_validation" "nlb" {
-  certificate_arn         = aws_acm_certificate.nlb.arn
-  validation_record_fqdns = [for r in aws_route53_record.nlb_cert_validation : r.fqdn]
 }
 
 data "aws_elb_hosted_zone_id" "current" {}
