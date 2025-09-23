612835ad3b5551cebb4dd8d7e6710cc8a97d8db3
rafiqirosli <fricheps@Hotmail.com>
2025-09-23 23:36:10 +0930

    Update nlb.tf

--- deploy/nlb.tf
+++ deploy/nlb.tf
@@ -46,7 +46,7 @@ resource "aws_route53_record" "nlb_alias" {
 
   alias {
     name                   = aws_lb.nlb.dns_name
-    zone_id                = data.aws_elb_hosted_zone_id.current.id
+    zone_id                = aws_lb.nlb.zone_id
     evaluate_target_health = false
   }
 }
