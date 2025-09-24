376f67ec820586e871058573f73cce215b18dbda
rafiqirosli <fricheps@Hotmail.com>
2025-09-24 19:06:15 +0930

    Update main.tf

--- deploy/modules/app/main.tf
+++ deploy/modules/app/main.tf
@@ -26,7 +26,7 @@ data "terraform_remote_state" "core" {
 
 provider "helm" {
   kubernetes = {
-    host                   = data.terraform_remote_state.core.outputs.cluster_name
+		host                   = data.terraform_remote_state.core.outputs.cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.core.outputs.cluster_certificate_authority_data)
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
