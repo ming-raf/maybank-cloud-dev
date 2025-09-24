cace8cd9ca5c7fe2b3e474024e8062fbc0eb03ea
rafiqirosli <fricheps@Hotmail.com>
2025-09-24 18:48:48 +0930

    Update main.tf

--- deploy/modules/eks/main.tf
+++ deploy/modules/eks/main.tf
@@ -27,18 +27,20 @@ data "terraform_remote_state" "core" {
 resource "aws_security_group" "eks" {
   name        = "${local.cluster_name}-sg"
   vpc_id     = data.terraform_remote_state.core.outputs.vpc_id
-  egress = [ {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  } ]
-  ingress = [ {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  } ]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_eks_cluster" "this" {
